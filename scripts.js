3a8422c921a2c7055a750a6c35cecc45f4cbee93
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-09-28 23:55:49 -0400

    Add player toggle functionality

--- scripts.js
+++ scripts.js
@@ -1,19 +1,29 @@
-let playerOne = {};
-let playerTwo = {};
+let player = [{}, {}];
+let activePlayer = player[0];
 
-const first = document.querySelector(".player-1");
-const second = document.querySelector(".player-2");
+let tl, tc, tr, cl, cc, cr, bl, bc, br;
+
+function swap() {
+    if (activePlayer === player[0]) {
+        activePlayer = player[1];
+    }
+    else if (activePlayer === player[1]) {
+        activePlayer = player[0];
+    }
+};
 
 const p1_form = document.querySelector(".pf-1");
 const p2_form = document.querySelector(".pf-2");
-
-const p1_name = document.querySelector(".p1-name");
-const p2_name = document.querySelector(".p2-name");
 
 const name1 = document.querySelector("#name-1");
 const symbol1 = document.querySelector("#sign-1");
 const name2 = document.querySelector("#name-2");
 const symbol2 = document.querySelector("#sign-2");
+
+const title = document.querySelector(".title");
+const table = document.querySelector(".board");
+const btns = document.querySelector(".buttons");
+const cell = document.querySelector(".cell");
 
 const Board = (function () {
     const board = [
@@ -23,61 +33,6 @@ const Board = (function () {
     ]
     return { board };
 })();
-// I know there MUST be a more efficient way of
-// handling this, but Im still working on it ;)
-const check = (function () {
-    if (Board.board[0][0] == 'X' &&
-        Board.board[0][1] == 'X' &&
-        Board.board[0][2] == 'X' ||
-        Board.board[0][0] == 'O' &&
-        Board.board[0][1] == 'O' &&
-        Board.board[0][2] == 'O' ||
-        Board.board[1][0] == 'X' &&
-        Board.board[1][1] == 'X' &&
-        Board.board[1][2] == 'X' ||
-        Board.board[1][0] == 'O' &&
-        Board.board[1][1] == 'O' &&
-        Board.board[1][2] == 'O' ||
-        Board.board[2][0] == 'X' &&
-        Board.board[2][1] == 'X' &&
-        Board.board[2][2] == 'X' ||
-        Board.board[2][0] == 'O' &&
-        Board.board[2][1] == 'O' &&
-        Board.board[2][2] == 'O' ||
-        Board.board[0][0] == 'X' &&
-        Board.board[1][0] == 'X' &&
-        Board.board[2][0] == 'X' ||
-        Board.board[0][0] == 'O' &&
-        Board.board[1][0] == 'O' &&
-        Board.board[2][0] == 'O' ||
-        Board.board[0][1] == 'X' &&
-        Board.board[1][1] == 'X' &&
-        Board.board[2][1] == 'X' ||
-        Board.board[0][1] == 'O' &&
-        Board.board[1][1] == 'O' &&
-        Board.board[2][1] == 'O' ||
-        Board.board[0][2] == 'X' &&
-        Board.board[1][2] == 'X' &&
-        Board.board[2][2] == 'X' ||
-        Board.board[0][2] == 'O' &&
-        Board.board[1][2] == 'O' &&
-        Board.board[2][2] == 'O' ||
-        Board.board[0][0] == 'O' &&
-        Board.board[1][1] == 'O' &&
-        Board.board[2][2] == 'O' ||
-        Board.board[0][0] == 'X' &&
-        Board.board[1][1] == 'X' &&
-        Board.board[2][2] == 'X' ||
-        Board.board[2][0] == 'O' &&
-        Board.board[1][1] == 'O' &&
-        Board.board[0][2] == 'O' ||
-        Board.board[2][0] == 'X' &&
-        Board.board[1][1] == 'X' &&
-        Board.board[0][2] == 'X'
-    ) {
-        alert("Game Over!");
-    }
-})();
 
 const game = (function () {
     const cell = function (pos, sign) {
@@ -85,9 +40,81 @@ const game = (function () {
     }
     return { cell };
 })();
+// I know there MUST be a more efficient way of
+// handling this, but Im still working on it ;)
+function check(pos) {
+    if (Board.board[0][0] == activePlayer.sign &&
+        Board.board[0][1] == activePlayer.sign &&
+        Board.board[0][2] == activePlayer.sign ||
+        Board.board[0][0] == activePlayer.sign &&
+        Board.board[0][1] == activePlayer.sign &&
+        Board.board[0][2] == activePlayer.sign ||
+        Board.board[1][0] == activePlayer.sign &&
+        Board.board[1][1] == activePlayer.sign &&
+        Board.board[1][2] == activePlayer.sign ||
+        Board.board[1][0] == activePlayer.sign &&
+        Board.board[1][1] == activePlayer.sign &&
+        Board.board[1][2] == activePlayer.sign ||
+        Board.board[2][0] == activePlayer.sign &&
+        Board.board[2][1] == activePlayer.sign &&
+        Board.board[2][2] == activePlayer.sign ||
+        Board.board[2][0] == activePlayer.sign &&
+        Board.board[2][1] == activePlayer.sign &&
+        Board.board[2][2] == activePlayer.sign ||
+        Board.board[0][0] == activePlayer.sign &&
+        Board.board[1][0] == activePlayer.sign &&
+        Board.board[2][0] == activePlayer.sign ||
+        Board.board[0][0] == activePlayer.sign &&
+        Board.board[1][0] == activePlayer.sign &&
+        Board.board[2][0] == activePlayer.sign ||
+        Board.board[0][1] == activePlayer.sign &&
+        Board.board[1][1] == activePlayer.sign &&
+        Board.board[2][1] == activePlayer.sign ||
+        Board.board[0][1] == activePlayer.sign &&
+        Board.board[1][1] == activePlayer.sign &&
+        Board.board[2][1] == activePlayer.sign ||
+        Board.board[0][2] == activePlayer.sign &&
+        Board.board[1][2] == activePlayer.sign &&
+        Board.board[2][2] == activePlayer.sign ||
+        Board.board[0][2] == activePlayer.sign &&
+        Board.board[1][2] == activePlayer.sign &&
+        Board.board[2][2] == activePlayer.sign ||
+        Board.board[0][0] == activePlayer.sign &&
+        Board.board[1][1] == activePlayer.sign &&
+        Board.board[2][2] == activePlayer.sign ||
+        Board.board[0][0] == activePlayer.sign &&
+        Board.board[1][1] == activePlayer.sign &&
+        Board.board[2][2] == activePlayer.sign ||
+        Board.board[2][0] == activePlayer.sign &&
+        Board.board[1][1] == activePlayer.sign &&
+        Board.board[0][2] == activePlayer.sign ||
+        Board.board[2][0] == activePlayer.sign &&
+        Board.board[1][1] == activePlayer.sign &&
+        Board.board[0][2] == activePlayer.sign
+    ) {
+        alert(`Game Over!\n
+        ${activePlayer.sign} won!`);
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                Board.board[i][j] = '';
+            }
+        }
+        game.cell(tl, '');
+        game.cell(tc, '');
+        game.cell(tr, '');
+        game.cell(cl, '');
+        game.cell(cc, '');
+        game.cell(cr, '');
+        game.cell(bl, '');
+        game.cell(bc, '');
+        game.cell(br, '');
+    }
+};
 
 const f1_button = document.querySelector(".f1-btn");
 f1_button.addEventListener("click", () => {
+    const first = document.querySelector(".player-1");
+    const p1_name = document.querySelector(".p1-name");
     p1_form.style.display = "none";
     p1_name.textContent = name1.value;
     let avatarOne = document.createElement("h1");
@@ -98,6 +125,8 @@ f1_button.addEventListener("click", () => {
 })
 const f2_button = document.querySelector(".f2-btn");
 f2_button.addEventListener("click", () => {
+    const second = document.querySelector(".player-2");
+    const p2_name = document.querySelector(".p2-name");
     p2_form.style.display = "none";
     p2_name.textContent = name2.value;
     let avatarTwo = document.createElement("h1");
@@ -105,45 +134,76 @@ f2_button.addEventListener("click", () => {
     avatarTwo.style.paddingTop = "16px";
     avatarTwo.style.color = "yellow";
     second.appendChild(avatarTwo);
+    table.style.height = "450px";
+    table.style.opacity = "1";
+    btns.style.height = "20svh";
+    title.style.opacity = "1";
 })
 
 const grid = (function () {
-    const tl = document.querySelector(".tl");
+    tl = document.querySelector(".tl");
     tl.addEventListener("click", () => {
-        game.cell(tl, playerOne.sign);
+        Board.board[0][0] = activePlayer.sign;
+        game.cell(tl, activePlayer.sign);
+        check();
     })
-    const tc = document.querySelector(".tc");
+    tl.addEventListener("click", swap);
+    tc = document.querySelector(".tc");
     tc.addEventListener("click", () => {
-        game.cell(tc, playerOne.sign);
+        Board.board[0][1] = activePlayer.sign;
+        game.cell(tc, activePlayer.sign);
+        check();
     })
-    const tr = document.querySelector(".tr");
+    tc.addEventListener("click", swap);
+    tr = document.querySelector(".tr");
     tr.addEventListener("click", () => {
-        game.cell(tr, playerOne.sign);
+        Board.board[0][2] = activePlayer.sign;
+        game.cell(tr, activePlayer.sign);
+        check();
     })
-    const cl = document.querySelector(".cl");
+    tr.addEventListener("click", swap);
+    cl = document.querySelector(".cl");
     cl.addEventListener("click", () => {
-        game.cell(cl, playerOne.sign);
+        Board.board[1][0] = activePlayer.sign;
+        game.cell(cl, activePlayer.sign);
+        check();
     })
-    const cc = document.querySelector(".cc");
+    cl.addEventListener("click", swap);
+    cc = document.querySelector(".cc");
     cc.addEventListener("click", () => {
-        game.cell(cc, playerOne.sign);
+        Board.board[1][1] = activePlayer.sign;
+        game.cell(cc, activePlayer.sign);
+        check();
     })
-    const cr = document.querySelector(".cr");
+    cc.addEventListener("click", swap);
+    cr = document.querySelector(".cr");
     cr.addEventListener("click", () => {
-        game.cell(cr, playerOne.sign);
+        Board.board[1][2] = activePlayer.sign;
+        game.cell(cr, activePlayer.sign);
+        check();
     })
-    const bl = document.querySelector(".bl");
+    cr.addEventListener("click", swap);
+    bl = document.querySelector(".bl");
     bl.addEventListener("click", () => {
-        game.cell(bl, playerOne.sign);
+        Board.board[2][0] = activePlayer.sign;
+        game.cell(bl, activePlayer.sign);
+        check();
     })
-    const bc = document.querySelector(".bc");
+    bl.addEventListener("click", swap);
+    bc = document.querySelector(".bc");
     bc.addEventListener("click", () => {
-        game.cell(bc, playerOne.sign);
+        Board.board[2][1] = activePlayer.sign;
+        game.cell(bc, activePlayer.sign);
+        check();
     })
-    const br = document.querySelector(".br");
+    bc.addEventListener("click", swap);
+    br = document.querySelector(".br");
     br.addEventListener("click", () => {
-        game.cell(br, playerOne.sign);
+        Board.board[2][2] = activePlayer.sign;
+        game.cell(br, activePlayer.sign);
+        check();
     })
+    br.addEventListener("click", swap);
 
     return {
         tl, tc, tr,
@@ -151,8 +211,6 @@ const grid = (function () {
         bl, bc, br
     }
 })();
-
-console.log(playerOne);
 
 function createPlayer(name, sign) {
     this.name = name,
@@ -164,8 +222,8 @@ const player1 = (function () {
     p1_form.addEventListener("submit", (event) => {
         event.preventDefault();
         let p1 = createPlayer(name1, symbol1);
-        playerOne.name = p1.name.value;
-        playerOne.sign = p1.sign.value;
+        player[0].name = p1.name.value;
+        player[0].sign = p1.sign.value;
         p1_form.reset();
         return { p1 };
     })
@@ -175,8 +233,8 @@ const player2 = (function () {
     p2_form.addEventListener("submit", (event) => {
         event.preventDefault();
         let p2 = createPlayer(name2, symbol2);
-        playerTwo.name = p2.name.value;
-        playerTwo.sign = p2.sign.value;
+        player[1].name = p2.name.value;
+        player[1].sign = p2.sign.value;
         p2_form.reset();
         return { p2 };
     })
@@ -184,21 +242,3 @@ const player2 = (function () {
 
 
 
-// const board = [
-//     ['X', 'O', 'O'],
-//     ['O', 'X', 'X'],
-//     ['O', 'X', 'O'],
-// ]
-
-// let count = 0;
-
-// for (let i = 0; i < 3; i++) {
-//     for (let j = 0; j < 3; j++) {
-//         if (board[i][j] == board[i][j + 1] && board[i][j] == board[i + 1][j]) {
-//             console.log("Game over!");
-//             count++;
-//         }
-//     }
-// }
-
-// console.log(count);
